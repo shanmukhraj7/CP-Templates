a0fe349323752a10448420348d640cd0416c00ba
shanmukhraj7 <148477598+shanmukhraj7@users.noreply.github.com>
2026-07-22 18:44:52 +0530

    Segment Tree Updated

--- Segment-Tree.cpp
+++ Segment-Tree.cpp
@@ -6,10 +6,21 @@ using namespace std;
 
 class SGT{
 public:
-    vi seg;
+    vi seg, lz;
     SGT(int n){
         seg.resize(4 * n + 5);
+        // lz.resize(4 * n + 5, 0);
     }
+
+    // void push(int idx, int low, int high){
+    //     if(lz[idx] == 0) return;
+    //     seg[idx] += lz[idx];
+    //     if(low != high){
+    //         lz[idx << 1] += lz[idx];
+    //         lz[idx << 1 | 1] += lz[idx];
+    //     }
+    //     lz[idx] = 0;
+    // }
 
     void build(int idx, int low, int high, vi& a){
         if(low == high){
@@ -23,6 +34,7 @@ public:
     }
 
     void update(int idx, int low, int high, int ui, int uv){
+        // push(idx, low, high);
         if(low == high){
             seg[idx] = uv;
             return;
@@ -38,11 +50,26 @@ public:
     }
 
     int query(int idx, int low, int high, int ql, int qr){
-        if(qr < low || high < ql) return INT_MAX;
+        // push(idx, low, high);
+        if(qr < low || high < ql) return LLONG_MIN;
         if(ql <= low && high <= qr) return seg[idx];
         int mid = low + ((high - low) >> 1);
         int left = query(idx << 1, low, mid, ql, qr);
         int right = query(idx << 1 | 1, mid + 1, high, ql, qr);
         return max(left, right);
     }
+
+    // void lzyupdate(int idx, int low, int high, int ql, int qr, int val){
+    //     push(idx, low, high);
+    //     if(high < ql || qr < low) return;
+    //     if(ql <= low && high <= qr){
+    //         lz[idx] += val;
+    //         push(idx, low, high);
+    //         return;
+    //     }
+    //     int mid = low + ((high - low) >> 1);
+    //     lzyupdate(idx << 1, low, mid, ql, qr, val);
+    //     lzyupdate(idx << 1 | 1, mid + 1, high, ql, qr, val);
+    //     seg[idx] = max(seg[idx << 1], seg[idx << 1 | 1]);
+    // }
 };
